4fa67560ae863d82eb69e8108fa12c6c6fd8e7d6
Aqua <kirillisthebest369@gmail.com>
2025-09-11 19:04:17 +0300

    modify task_1.py

---
+++ task_1.py
@@ -0,0 +1,17 @@
+def get_index_of_sum(nums, target):
+    for l in range(len(nums)):
+        for r in range(l + 1, len(nums)):
+            if nums[l] + nums[r] > target:
+                break
+            if nums[l] + nums[r] == target:
+                return f'Indexes: [{l}, {r}]'
+
+    return "No indexes found"
+
+
+try:
+    a = list(map(int, input("Write your array with elements divided by commas: ").split(',')))
+    target = int(input("Write your target number: "))
+    print(get_index_of_sum(a, target))
+except Exception:
+    print("You wrote bullshit!")
