f60fb24eb86e3e4eb5740479429eff4ee81905ba
Aqua <kirillisthebest369@gmail.com>
2025-12-07 18:57:13 +0300

    solve task_7

---
+++ task_7/sol.py
@@ -0,0 +1,111 @@
+import sys
+import functools
+import requests
+import io
+import logging
+
+from typing import Callable
+
+
+def logger(func: Callable=None, *, handle=sys.stdout) -> Callable:
+    """ Параметризуемый декоратор, который ведёт логирование в зависимости от аргумента handle
+
+    :param func: декорируемая функция
+    :param handle: поток ввода (куда будет произведено логирование)
+    :return: возвращает задекорированную функцию
+    :rtype: Callable
+    """
+    if func is None:
+        return lambda func: logger(func, handle=handle)
+
+    def log_info(message):
+        """ Печатает сообщение message о выполнении кода с учётом потока handle (логирование)
+
+        :param message: сообщение, которое печатается
+        """
+        if isinstance(handle, logging.Logger):
+            handle.info(message)
+        else:
+            handle.write("INFO: " + message + "\n")
+
+    def log_error(message):
+        """ Печатает сообщение message об ошибке с учётом потока handle (логирование)
+
+        :param message: сообщение, которое печатается
+        """
+        if isinstance(handle, logging.Logger):
+            handle.error(message)
+        else:
+            handle.write("ERROR: " + message + "\n")
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        """ Функция обёртка
+
+        Логируется сообщение о выполнении кода, дальше в зависимости от результата выполнения
+        сообщается об успешном выполнении или выбрасывается исключение об ошибке
+        """
+        log_info(f"Execute: {func.__name__} args={args}, kwargs={kwargs}")
+
+        try:
+            result = func(*args, **kwargs)
+            log_info(f"Success: {func.__name__} -> {result}")
+            return result
+
+        except Exception as e:
+            log_error(f"Exception in {func.__name__}: {type(e).__name__}: {e}")
+            raise e
+
+    return wrapper
+
+
+# настройка логгера
+logging.basicConfig(
+    filename="currency_file.log",
+    level=logging.DEBUG,
+    format="%(levelname)s: %(message)s"
+)
+
+# куда будет логироваться информация
+file_logger = logging.getLogger("currency_file")
+
+
+@logger(handle=file_logger)
+def get_currencies(currency_codes: list, url:str='https://www.cbr-xml-daily.ru/daily_json.js')->dict:
+    """
+    Получает курсы валют с API Центробанка России.
+
+    Args:
+        currency_codes (list): Список символьных кодов валют (например, ['USD', 'EUR']).
+
+    Returns:
+        dict: Словарь, где ключи - символьные коды валют, а значения - их курсы.
+              Возвращает None в случае ошибки запроса.
+    """
+    try:
+        response = requests.get(url)
+    except requests.RequestException as e:
+        raise ConnectionError(f"API unavailable: {e}")
+
+    try:
+        data = response.json()
+    except ValueError as e:
+        raise ValueError(f"Invalid JSON: {e}")
+
+    currencies = {}
+
+    if "Valute" not in data:
+        raise KeyError(f'Key "Value" not found')
+
+    for code in currency_codes:
+        if code not in data["Valute"]:
+            raise KeyError(f"Currency '{code}' not found")
+
+        value = data["Valute"][code]["Value"]
+
+        if not isinstance(value, (int, float)):
+            raise TypeError(f"Invalid type for currency {code}: {type(value)}")
+
+        currencies[code] = value
+
+    return currencies
---
+++ task_7/test.py
@@ -0,0 +1,117 @@
+import unittest
+import io
+import logging
+from unittest.mock import patch, MagicMock
+
+from sol import get_currencies, logger
+
+# Я использовал unittest.mock для того, чтобы создавать имитацию работы request.get, поскольку
+# функция get_currencies зависит от requests.get (ведь информация о валютах непостоянна), поэтому
+# для проверки работы функции мы имитируем requests.get и настраиваем под свои нужды
+
+
+# Тестирование функции get_currencies
+class TestGetCurrencies(unittest.TestCase):
+    # Возврат реальных курсов
+    @patch("sol.requests.get")
+    def test_success(self, mock_get):
+        mock_resp = MagicMock()
+        mock_resp.json.return_value = {
+            "Valute": {
+                "USD": {"Value": 76.2},
+                "EUR": {"Value": 98.3}
+            }
+        }
+        mock_resp.raise_for_status.return_value = None
+        mock_get.return_value = mock_resp
+
+        result = get_currencies(["USD", "EUR"])
+
+        self.assertEqual(result, {"USD": 76.2, "EUR": 98.3})
+
+    # Несуществующая валюта
+    @patch("sol.requests.get")
+    def test_currency_not_found(self, mock_get):
+        mock_resp = MagicMock()
+        mock_resp.json.return_value = {"Valute": {"USD": {"Value": 90.1}}}
+        mock_resp.raise_for_status.return_value = None
+        mock_get.return_value = mock_resp
+
+        with self.assertRaises(KeyError):
+            get_currencies(["EUR"])
+
+    # Нет подключения
+    @patch("sol.requests.get")
+    def test_connection_error(self, mock_get):
+        mock_get.side_effect = Exception("Network down")
+
+        with self.assertRaises(ConnectionError):
+            get_currencies(["USD"])
+
+    # Некорректный JSON
+    @patch("sol.requests.get")
+    def test_invalid_json(self, mock_get):
+        mock_resp = MagicMock()
+        mock_resp.raise_for_status.return_value = None
+        mock_resp.json.side_effect = ValueError("Invalid JSON")
+
+        mock_get.return_value = mock_resp
+
+        with self.assertRaises(ValueError):
+            get_currencies(["USD"])
+
+    # Отсутствует ключ "Valute"
+    @patch("sol.requests.get")
+    def test_missing_valute_key(self, mock_get):
+        mock_resp = MagicMock()
+        mock_resp.json.return_value = {"SomethingElse": {}}
+        mock_resp.raise_for_status.return_value = None
+        mock_get.return_value = mock_resp
+
+        with self.assertRaises(KeyError):
+            get_currencies(["USD"])
+
+
+# Тестирование декоратора
+class TestLoggerDecorator(unittest.TestCase):
+    def setUp(self):
+        self.stream = io.StringIO()
+
+        @logger(handle=self.stream)
+        def f(x):
+            return x * 2
+
+        @logger(handle=self.stream)
+        def f_err(x):
+            raise ValueError("error")
+
+        self.f = f
+        self.f_err = f_err
+
+    # Успешное логирование
+    def test_logger_success(self):
+        result = self.f(5)
+        logs = self.stream.getvalue()
+
+        self.assertEqual(result, 10)
+
+        self.assertRegex(logs, "INFO")
+        self.assertRegex(logs, "Execute")
+        self.assertRegex(logs, "Success")
+        self.assertRegex(logs, "5")
+        self.assertRegex(logs, "10")
+
+    # Ошибка в логировании
+    def test_logger_error(self):
+        with self.assertRaises(ValueError):
+            self.f_err(7)
+
+        logs = self.stream.getvalue()
+
+        self.assertRegex(logs, "ERROR")
+        self.assertRegex(logs, "ValueError")
+        self.assertRegex(logs, "error")
+
+
+if __name__ == "__main__":
+    unittest.main()
